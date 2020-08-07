829e5a35c7508f9127eebe448fd7139bf5dd746a
Michael Gorianskyi <goreanski@gmail.com>
2020-08-07 18:39:41 +0200

    Added a dangling comma

--- src/lambda/ip-index/index.js
+++ src/lambda/ip-index/index.js
@@ -28,7 +28,7 @@ async function handler(event) {
       'X-Requested-With': '*',
       'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with',
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,OPTIONS'
+      'Access-Control-Allow-Methods': 'GET,OPTIONS',
     },
     statusCode: 200,
     body: JSON.stringify(data),
