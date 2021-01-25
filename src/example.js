6422040780ad3c01862e1fdef56a39eee86dc55e
Michael Gorianskyi <goreanski@gmail.com>
2021-01-25 18:23:46 +0100

    Removed commented asns

--- src/example.js
+++ src/example.js
@@ -6,7 +6,7 @@ console.time('init');
 const ipIndex = new IpIndex(`${__dirname}/../dist/ip-index.db`);
 console.timeEnd('init');
 
-const ip = '93.201.96.204';
+const ip = '1.1.1.1';
 
 console.time('queries');
 console.log('Datacenter:', ipIndex.isDatacenter(ip));
