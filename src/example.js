f4435be46d08dfc1f33ed649d155bfa9d66d5cfa
Michael Gorianskyi <goreanski@gmail.com>
2021-12-25 20:25:08 +0100

    Searching for multiple comma separated IPs

--- src/example.js
+++ src/example.js
@@ -1,6 +1,6 @@
 const { getIpInfo } = require('./index');
-const ip = '199.103.22.0';
+const ip = '199.103.22.0,1.1.1.1';
 
 console.time('time');
-console.log(ip, getIpInfo(ip));
+console.log(getIpInfo(ip));
 console.timeEnd('time');
--- src/index.js
+++ src/index.js
@@ -43,31 +43,31 @@ const ranges = readFileSync('../data/asns_cidrs.csv').toString()
   });
 
 function ipToInt(ip) {
-  return ip.split('.').reduce((int, oct) => (int << 8) + parseInt(oct, 10), 0) >>> 0;
+  return ip.trim().split('.').reduce((int, oct) => (int << 8) + parseInt(oct, 10), 0) >>> 0;
 }
 
 function getIpInfo(ip) {
-  const ipInt = ipToInt(ip);
-  let asn = ranges
-    .filter((range) => {
-      if (!range) {
-        return false;
+  const ips = ip.split(',');
+  const ipsInt = ips.map(ipToInt);
+  const matches = [];
+
+  ranges.forEach((range) => {
+    if (!range) {
+      return false;
+    }
+
+    for (let index in ipsInt) {
+      if (+range.start <= ipsInt[index] && +range.end >= ipsInt[index]) {
+        const res = { ip: ips[index], ...range };
+        delete res.start;
+        delete res.end;
+
+        matches.push(res);
       }
+    }
+  });
 
-      return range.start <= ipInt && range.end >= ipInt;
-    })
-    .sort((a, b) => a.end - a.start >= b.end - b.start ? 1 : -1)
-    .shift();
-
-  if (!asn) {
-    return null;
-  }
-
-  const res = { ...asn };
-  delete res.start;
-  delete res.end;
-
-  return res;
+  return matches;
 }
 
 module.exports = {
