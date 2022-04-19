d274276d53c60d5e389d36f47317453501aafc43
Michael Gorianskyi <goreanski@gmail.com>
2022-04-19 21:58:18 +0200

    Added update script

--- bin/post-install.js
+++ bin/post-install.js
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const fileAsns = createWriteStream(`${__dirname}/../data/asns.csv`);
 const fileCidrs = createWriteStream(`${__dirname}/../data/asns_cidrs.csv`);
+const fileNord = createWriteStream(`${__dirname}/../data/ips_nord.csv`);
 const opts = { responseType: 'blob' };
 
 axios.get('https://github.com/Umkus/asn-ip/releases/download/latest/as.csv', opts)
@@ -14,3 +15,6 @@ axios.get('https://github.com/Umkus/asn-ip/releases/download/latest/as.csv', opt
 
 axios.get('https://github.com/Umkus/asn-ip/releases/download/latest/ranges_ipv4.csv', opts)
   .then((res) => fileCidrs.write(res.data));
+
+axios.get('https://github.com/Umkus/nordvpn-ips/releases/download/ips/ips.csv', opts)
+  .then((res) => fileNord.write(res.data));
---
+++ src/update.js
@@ -0,0 +1,92 @@
+import {
+  readFileSync,
+  writeFileSync,
+} from 'fs';
+import { getIpInfo } from './index.js';
+
+const csvAsns = readFileSync('../data/asns.csv').toString()
+  .split(/\n/)
+  .filter((item) => item.trim().length)
+  .map((row) => {
+    const items = row.split(',', 3);
+    items[0] = +items[0];
+    items[2] = items[2].replace('"', '');
+    return items;
+  });
+csvAsns.shift();
+
+const patternsBad = [
+  /(^|\W)vps(\W|$)/i,
+  /anonim/i,
+  /anony/i,
+  /cdn(\W|$)/i,
+  /coll?o(c|[^a-z]|$)/i,
+  /scale/i,
+  /cloud/i,
+  /data.?center/i,
+  /ddos/i,
+  /dedi/i,
+  /host/i,
+  /server/i,
+  /vpn/i,
+  /layer/i,
+  /\.(com|org|de|ru)(\W|$)/i,
+];
+
+const patternsGood = [
+  /(\W|^)mobile/i,
+  /(\W|^)tv(\W|$)/i,
+  /afrihost/i,
+  /broad/i,
+  /vcable/i,
+  /fiber/i,
+  /wifi/i,
+  /home(\W|$)/i,
+  /internet/i,
+  /isp/i,
+  /dsl/i,
+  /tele/i,
+  /echostar/i,
+  /vodafone/i,
+  /clouditalia/i,
+  /centurylink/i,
+  /communic/i,
+];
+
+const nordIps = readFileSync('../data/ips_nord.csv').toString().split(/\n/);
+
+const badAsns = csvAsns.filter((item) => {
+  for (let patBad of patternsBad) {
+    if (patBad.test(`${item[1]} ${item[2]}`)) {
+      for (let patGood of patternsGood) {
+        if (patGood.test(`${item[1]} ${item[2]}`)) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+  }
+
+  return false;
+});
+
+const newBad = badAsns.map((item) => +item[0]);
+const oldBad = new Set(readFileSync('../data/asns_dcs.csv').toString().split(/\n/).map((item) => +item));
+
+newBad.forEach((item) => {
+  if (!oldBad.has(item)) {
+    oldBad.add(item);
+  }
+});
+
+nordIps.forEach((nordIp) => {
+  getIpInfo(nordIp)
+    .forEach((item) => {
+      if (!item.hosting) {
+        oldBad.add(item.asn);
+      }
+    });
+});
+
+writeFileSync('../data/asns_dcs.csv', [...oldBad].sort().join('\n') + '\n');
