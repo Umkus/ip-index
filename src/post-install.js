18b5981db5645f00bc240d29bbfce1c4b3cbd740
Michael Gorianskyi <goreanski@gmail.com>
2022-05-26 11:52:45 +0200

    Using writeFileSync for post install

--- src/post-install.js
+++ src/post-install.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
-import { createWriteStream, readFileSync, writeFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const fileAsns = createWriteStream(`${__dirname}/../data/asns.csv`);
-const fileCidrs = createWriteStream(`${__dirname}/../data/asns_cidrs.csv`);
-const fileNord = createWriteStream(`${__dirname}/../data/ips_nord.csv`);
+const fileAsns = `${__dirname}/../data/asns.csv`;
+const fileCidrs = `${__dirname}/../data/asns_cidrs.csv`;
+const fileNord = `${__dirname}/../data/ips_nord.csv`;
 const opts = { responseType: 'blob' };
 
 console.log('Building data files, please wait...');
 
 const promises = [
-  axios.get('https://github.com/Umkus/asn-ip/releases/download/latest/as.csv', opts).then((res) => fileAsns.write(res.data)),
-  axios.get('https://github.com/Umkus/asn-ip/releases/download/latest/ranges_ipv4.csv', opts).then((res) => fileCidrs.write(res.data)),
-  axios.get('https://github.com/Umkus/nordvpn-ips/releases/download/ips/ips.csv', opts).then((res) => fileNord.write(res.data)),
+  axios.get('https://github.com/Umkus/asn-ip/releases/download/latest/as.csv', opts).then((res) => writeFileSync(fileAsns, res.data)),
+  axios.get('https://github.com/Umkus/asn-ip/releases/download/latest/ranges_ipv4.csv', opts).then((res) => writeFileSync(fileCidrs, res.data)),
+  axios.get('https://github.com/Umkus/nordvpn-ips/releases/download/ips/ips.csv', opts).then((res) => writeFileSync(fileNord, res.data)),
 ];
 
 const countries = readFileSync(`${__dirname}/../node_modules/@ip-location-db/asn-country/asn-country-ipv4-num.csv`).toString().split('\n')
