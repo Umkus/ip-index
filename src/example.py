adecd8d4f9b02d9fabbd1508a7282cd241d6e6c1
Mykhailo Gorianskyi <goreanski@gmail.com>
2024-10-17 22:49:21 +0200

    updated example

--- src/example.py
+++ src/example.py
@@ -7,5 +7,5 @@ data_path = os.path.join(current_dir, '..', 'data')
 
 m = maxminddb.open_database(f"{data_path}/ip-index.mmdb")
 r = m.get('1.1.1.1')
-
+assert r == {'asn': 13335, 'country': 'US', 'asn_name': 'CLOUDFLARENET', 'hosting': 1}
 print(r)
